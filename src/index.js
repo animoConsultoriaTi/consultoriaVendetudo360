6a0e5685628edb07891289a1ef72d40705446bb9
Nathan-Barbosa <nathan.s.barbosa01@gmail.com>
2022-05-15 23:40:13 -0300

    finalizado feature/questions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles/styles.scss';
+
+const accordion = document.querySelectorAll('.accordion');
+console.log(accordion);
+
+accordion.forEach((item) => {
+    const iconButton =     item.querySelector('.icon-plus')
+    iconButton.addEventListener('click', () =>{
+        item.classList.toggle('active');
+    })
+})
